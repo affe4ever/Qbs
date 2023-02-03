742078c51b195ad4e5204579d68a339d4463c2d3
affe4ever <93663446+affe4ever@users.noreply.github.com>
2023-02-03 14:47:16 +0100

    badoo

--- Assets/Scripts/Movemont.cs
+++ Assets/Scripts/Movemont.cs
@@ -7,7 +7,7 @@ public class Movemont : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Physics.gravity = new Vector3(0, -1.0F, 0);
     }
 
     // Update is called once per frame
