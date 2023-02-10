ef926b703511a4029dea57045964b77c8fd85045
affe4ever <93663446+affe4ever@users.noreply.github.com>
2023-02-10 14:59:00 +0100

    badoobadoododo

--- Assets/Scripts/Changegravity.cs
+++ Assets/Scripts/Changegravity.cs
@@ -4,32 +4,53 @@ using UnityEngine;
 
 public class Changegravity : MonoBehaviour
 {
-    private bool isChanged = false;
+    public bool isSpacePressed;
+    public bool gravityReversed;
+    public bool isGrounded;
     private float force = 9.8f;
     public GameObject character;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gravityReversed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space))
-        {
-            if (isChanged) {
-                Physics.gravity = new Vector3(0, -force, 0);
-                isChanged = false;
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            isSpacePressed = true;
+        }
+        else{
+            isSpacePressed = false;
+        }
+    }
+
+    void FixedUpdate() {
+        if (isSpacePressed){
+            if (gravityReversed){
+                gravityReversed = false;
             }
-            else
-            {
-                Physics.gravity = new Vector3(0, force, 0);
-                isChanged = true;
+            else {
+                gravityReversed = true; 
             }
-            //character.gravity = new Vector3(0, force, 0);
-            //Physics.gravity = new Vector3(0, -force, 0);   
+        }
+
+        if (gravityReversed) {
+            rb.velocity = new Vector3(0, force, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Flooor"){
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit(Collision collision) {
+        if (collision.gameObject.tag == "Flooor"){
+            isGrounded = false;
         }
     }
 }
--- Assets/Scripts/Movemont.cs
+++ Assets/Scripts/Movemont.cs
@@ -6,6 +6,10 @@ public class Movemont : MonoBehaviour
 {
 	public float speed = 2.0f;
 	public GameObject character;
+	public bool isRight;
+	public bool isLeft;
+	public bool isForward;
+	public bool isBack;
 
     // Start is called before the first frame update
     void Start()
@@ -16,24 +20,50 @@ public class Movemont : MonoBehaviour
     // Update is called once per frame
 
 
-void Update () {
+	void Update () {
 		
 		if (Input.GetKey(KeyCode.UpArrow)){
-			transform.position += Vector3.right * speed * Time.deltaTime;
+			isForward = true;
+		}
+		else{
+			isForward = false;
 		}
 		if (Input.GetKey(KeyCode.DownArrow)){
-			transform.position += Vector3.left* speed * Time.deltaTime;
+			isBack = true;
+		}
+		else{
+			isBack = false;
 		}
 		if (Input.GetKey(KeyCode.LeftArrow)){
-			transform.position += Vector3.forward * speed * Time.deltaTime;
+			isLeft = true;
+		}
+		else{
+			isLeft = false;
 		}
 		if (Input.GetKey(KeyCode.RightArrow)){
-			transform.position += Vector3.back* speed * Time.deltaTime;
+			isRight = true;
+		}
+		else {
+			isRight = false;
 		}
 	}
     
-}
 
+	void FixedUpdate(){
+		if (isForward){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (isBack){
+			transform.position += Vector3.left* speed * Time.deltaTime;
+		}
+		if (isLeft){
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+		if (isRight){
+			transform.position += Vector3.back* speed * Time.deltaTime;
+		}
+	}
+}
 /*
 		if (Input.GetKey(KeyCode.RightArrow)){
 			transform.position += Vector3.right * speed * Time.deltaTime;
