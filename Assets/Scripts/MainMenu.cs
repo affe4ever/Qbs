1a985ab64aba6368aa0aeaaa6f2067bbaa539573
affe4ever <93663446+affe4ever@users.noreply.github.com>
2023-02-13 15:37:36 +0100

    lil update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,8 @@ public class NewBehaviourScript : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
+    
+    public void QuitGame(){
+        Application.Quit();
+    }
 }
