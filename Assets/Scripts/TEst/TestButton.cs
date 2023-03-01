f5aaa35ef18af31d5c0aae1d9d6c4be11135b9bb
affe4ever <93663446+affe4ever@users.noreply.github.com>
2023-03-01 21:48:51 +0100

    raycasting

--- Assets/Scripts/TEst/TestButton.cs
+++ Assets/Scripts/TEst/TestButton.cs
@@ -6,33 +6,51 @@ public class TestButton : MonoBehaviour
 {
     private AudioSource sound;
     public GameObject door;
+    private Vector3 direction = Vector3.up;
+    private float range = 0.6f;
+    private bool buttonPressed = false;
+    /*
+    private BoxCollider BC;
+    private Vector3 topRight;
+    private Vector3 bottomRight;
+    private Vector3 topLeft;
+    private Vector3 bottomLeft;
+    */
 
     // Start is called before the first frame update
     void Start()
     {
         sound = GetComponent<AudioSource>();
         door.GetComponent<TestDoor>().amount ++;
+        //BC = GetComponent<BoxCollider>();
+        //topRight = BC.size;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.DrawRay(topRight, transform.TransformDirection(direction * range));
     }
     
     private void OnTriggerEnter(Collider other){
-        if (other.gameObject.tag == "ActivePlayer" || other.gameObject.tag == "Interactable" || other.gameObject.tag == "Player")
-        {
-            sound.Play();
-            door.GetComponent<TestDoor>().pressed ++;
-        }
+
+            if ((other.gameObject.tag == "ActivePlayer" || other.gameObject.tag == "Interactable" || other.gameObject.tag == "Player") && !buttonPressed)
+            {
+                sound.Play();
+                door.GetComponent<TestDoor>().pressed++;
+                buttonPressed = true;
+            }
     }
 
     private void OnTriggerExit(Collider other){
-        if (other.gameObject.tag == "ActivePlayer" || other.gameObject.tag == "Interactable" || other.gameObject.tag == "Player")
+        if ((other.gameObject.tag == "ActivePlayer" || other.gameObject.tag == "Interactable" || other.gameObject.tag == "Player") && buttonPressed)
         {
-            sound.Play();
-            door.GetComponent<TestDoor>().pressed --;
+            Ray theRay = new(transform.position, transform.TransformDirection(direction * range));
+            if (!Physics.Raycast(theRay, out RaycastHit hit, range)) {
+                sound.Play();
+                door.GetComponent<TestDoor>().pressed--;
+                buttonPressed = false;
+            }      
         }
     }
 }
