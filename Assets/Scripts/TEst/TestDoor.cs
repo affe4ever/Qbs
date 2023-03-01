cd1c1f5d1c1dd4d6ab966cecbf3269f7b1acd5a0
affe4ever <93663446+affe4ever@users.noreply.github.com>
2023-03-01 20:14:43 +0100

    a lot

--- Assets/Scripts/TEst/TestDoor.cs
+++ Assets/Scripts/TEst/TestDoor.cs
@@ -52,8 +52,8 @@ public class TestDoor : MonoBehaviour
         openSound.Play();
         open = true;
         this.gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).GetComponent<Light>().color = Color.green;
-        this.gameObject.transform.GetChild(1).localPosition = new Vector3(0, 0, +1.0f);
-        this.gameObject.transform.GetChild(2).localPosition = new Vector3(0, 0, -1.0f);
+        this.gameObject.transform.GetChild(1).position += new Vector3(0, 0, 1.6f);
+        this.gameObject.transform.GetChild(2).position -= new Vector3(0, 0, 1.6f);
     }
 
     private void CloseDoor()
